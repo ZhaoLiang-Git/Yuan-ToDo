7743945e0fc26f1b6e58a6801ca23c0f1cab4dc6
jsyjst <y8588130@163.com>
2019-06-07 22:38:34 +0800

    添加引导页，并在引导页做了cookie判断处理

---
+++ app/src/main/java/com/example/schedulemanagement/view/activity/LeadActivity.java
@@ -0,0 +1,55 @@
+package com.example.schedulemanagement.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.schedulemanagement.R;
+import com.example.schedulemanagement.app.Constants;
+import com.example.schedulemanagement.base.entity.BaseResponse;
+import com.example.schedulemanagement.callback.BaseResponseCallback;
+import com.example.schedulemanagement.entity.LoginAndRegister;
+import com.zhy.http.okhttp.OkHttpUtils;
+
+public class LeadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lead);
+        //ck();
+        toMainActivity();
+    }
+    private void ck(){
+        OkHttpUtils.post()
+                .url(Constants.BASE_URL+"ck")
+                .build()
+                .execute(new BaseResponseCallback<LoginAndRegister>() {
+                    @Override
+                    public void onResponse(BaseResponse response, int id) {
+                        if(response.getCode() == Constants.CODE_SUCCESS){
+                            toMainActivity();
+                        }else {
+                            toLoginActivity();
+                        }
+                    }
+                });
+    }
+    public void toMainActivity(){
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(LeadActivity.this, MainActivity.class));
+            overridePendingTransition(R.anim.anim_launch_enter, 0);
+            finish();
+        }, 2000);
+    }
+    public void toLoginActivity(){
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(LeadActivity.this, LoginActivity.class));
+            overridePendingTransition(R.anim.anim_launch_enter, 0);
+            finish();
+        }, 2000);
+    }
+}
